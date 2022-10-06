6bd27ca19ea441a13a2f40563436ef9bda74cc7d
King Wisdom <kingwisdom.dev@gmail.com>
2022-10-06 14:34:08 +0700

    fe user: do base components

---
+++ frontend/components/Base/DropDown/index.tsx
@@ -0,0 +1,62 @@
+import { Menu, Transition } from "@headlessui/react"
+import clsx from "clsx"
+import Image from "next/image"
+import { Fragment } from "react"
+
+type DropDownProps = {
+  items?: any[]
+  label?: any
+  selectedItem?: any
+}
+
+const DropDown = (props: DropDownProps) => {
+  const { items = [], label, selectedItem } = props
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-between items-center rounded-xl bg-white px-3 py-2.5 text-base font-semibold">
+          {selectedItem?.label || label}
+          <Image
+            className="cursor-pointer"
+            src="/images/icon-down.svg"
+            width={14}
+            height={8}
+            alt=""
+          />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 left-0 z-10 mt-2 origin-top-right rounded-xl bg-white">
+          <div className="py-1">
+            {items.map((item) => (
+              <Menu.Item key={item.value}>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={clsx(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    {item.label}
+                  </a>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
+
+export default DropDown
--- frontend/components/Base/Pagination/index.tsx
+++ frontend/components/Base/Pagination/index.tsx
@@ -1,7 +1,87 @@
-import React from "react"
+import clsx from "clsx"
+import Image from "next/image"
+import Link from "next/link"
+import ButtonLink from "../ButtonLink"
 
-const Pagination = () => {
-  return <div>Pagination</div>
+type PaginationProps = {
+  className?: string
+  currentPage: number
+}
+
+const fakePagination = {
+  startPage: 1,
+  endPage: 12
+}
+
+const Pagination = (props: PaginationProps) => {
+  const { className = "", currentPage } = props
+  return (
+    <div className={clsx("flex items-center", className)}>
+      <ButtonLink
+        className={clsx(
+          fakePagination.startPage === 1 && "opacity-50",
+          "bg-birdRed w-8 h-8 justify-center"
+        )}
+        href="#"
+      >
+        <Image
+          className="cursor-pointer"
+          src="/images/icon-previous.svg"
+          width={14}
+          height={8}
+          alt=""
+        />
+      </ButtonLink>
+      <div className="flex mx-5">
+        {Array(
+          fakePagination.endPage - fakePagination.startPage > 3
+            ? 3
+            : fakePagination.endPage - fakePagination.startPage
+        )
+          .fill(1)
+          .map((item, index) => (
+            <Link href="#" key={index}>
+              <a
+                className={clsx(
+                  currentPage === fakePagination.startPage + index &&
+                    "text-birdRed",
+                  "w-8 text-center text-xl font-semibold hover:text-birdRed transition "
+                )}
+              >
+                {fakePagination.startPage + index}
+              </a>
+            </Link>
+          ))}
+
+        {fakePagination.endPage - fakePagination.startPage > 3 && (
+          <Link href="#">
+            <a className="w-8 text-center text-xl font-semibold hover:text-birdRed transition ">
+              ...
+            </a>
+          </Link>
+        )}
+        <Link href="#">
+          <a
+            className={clsx(
+              currentPage === fakePagination.endPage && "text-birdRed",
+              "w-8 text-center text-xl font-semibold hover:text-birdRed transition "
+            )}
+          >
+            {fakePagination.endPage}
+          </a>
+        </Link>
+      </div>
+      <ButtonLink className="bg-birdRed w-8 h-8 justify-center" href="">
+        <Image
+          className="cursor-pointer"
+          src="/images/icon-next.svg"
+          width={14}
+          height={8}
+          alt=""
+        />
+      </ButtonLink>
+    </div>
+  )
 }
 
 export default Pagination
