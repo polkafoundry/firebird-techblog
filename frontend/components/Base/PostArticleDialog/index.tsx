84cb6abb4da2e14c3e3de19116c09fb09b58ac30
King Wisdom <kingwisdom.dev@gmail.com>
2022-10-14 09:58:55 +0700

    Revert "fe: add icon link"
    This reverts commit fb341a9678daf8ab42073fdf9369718b33f01624.

--- frontend/components/Base/PostArticleDialog/index.tsx
+++ frontend/components/Base/PostArticleDialog/index.tsx
@@ -26,7 +26,6 @@ import {
 import styles from "./post.module.scss"
 import iconBack from "/public/images/icon-back.svg"
 import iconView from "/public/images/icon-view.svg"
-import iconLink from "/public/images/icon-link-gray.svg"
 
 const inputStyles = clsx(
   "flex w-full h-[52px] px-5 justify-center bg-[#F7F8F9] rounded-lg border border-[#DEDEDE] text-sm",
@@ -121,12 +120,8 @@ const PostArticleDialog = (props: DialogTypes) => {
   }
 
   const renderEditForm = () => {
-    const renderInputField = (
-      fieldName: any,
-      placeholder: string,
-      leftIcon?: any
-    ) => (
-      <div className="relative">
+    const renderInputField = (fieldName: any, placeholder: string) => (
+      <div>
         <input
           type="text"
           name={fieldName}
@@ -140,9 +135,6 @@ const PostArticleDialog = (props: DialogTypes) => {
             {errorFormData[fieldName as keyof FormErrorDataTypes]}
           </span>
         )}
-        <div className="absolute top-[10px] right-[10px]">
-          <Image src={leftIcon} alt="" />
-        </div>
       </div>
     )
 
@@ -225,7 +217,7 @@ const PostArticleDialog = (props: DialogTypes) => {
         </div>
 
         {renderInputField("title", "Your post title *")}
-        {renderInputField("author_image", "Your avatar image link", iconLink)}
+        {renderInputField("author_image", "Your avatar image link")}
 
         <span className={clsx("mt-3 font-semibold", "xs:mt-2")}>
           Choose Categories
