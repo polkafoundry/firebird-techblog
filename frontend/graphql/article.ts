628f15bc89885d06cb3bf7f1f08d1e0c3154d8f5
King Wisdom <kingwisdom.dev@gmail.com>
2022-10-18 10:51:21 +0700

    fe: add queries

--- frontend/graphql/article.ts
+++ frontend/graphql/article.ts
@@ -58,3 +58,96 @@ export const GET_TOP_LASTEST_ARTICLES = gql`
     )
   }
 `
+
+export const SEARCH_ARTICLES = gql`
+  query Articles($title: String!, $take: Int!) {
+    articles(
+      where: { is_display: { equals: 1 }, title: { contains: $title } }
+      orderBy: { created_at: desc }
+      take: $take
+    ) {
+      id
+      title
+    }
+  }
+`
+
+export const GET_EXCLUSIVE_ARTICLES = gql`
+  query Articles($take: Int!, $skip: Int) {
+    exclusive: articles(
+      where: { is_display: { equals: 1 }, is_exclusive: { equals: true } }
+      orderBy: { created_at: desc }
+      take: $take
+    ) {
+      id
+      title
+      author_name
+      author_image
+      thumbnail {
+        url
+      }
+      category {
+        id
+        name
+      }
+      content
+      created_at
+    }
+    lastest: articles(
+      where: { is_display: { equals: 1 } }
+      orderBy: { created_at: desc }
+      take: $take
+      skip: $skip
+    ) {
+      id
+      title
+      author_name
+      author_image
+      thumbnail {
+        url
+      }
+      category {
+        id
+        name
+      }
+      content
+      created_at
+    }
+  }
+`
+
+export const GET_BANNER = gql`
+  query {
+    aMABanners {
+      id
+      banner
+    }
+  }
+`
+export const GET_RELATED_ARTICLES = gql`
+  query Articles($category: CategoryManyRelationFilter, $id: ID, $take: Int!) {
+    articles(
+      where: {
+        is_display: { equals: 1 }
+        category: $category
+        id: { not: { equals: $id } }
+      }
+      orderBy: { created_at: desc }
+      take: $take
+    ) {
+      id
+      title
+      author_name
+      author_image
+      thumbnail {
+        url
+      }
+      category {
+        id
+        name
+      }
+      content
+      created_at
+    }
+  }
+`
